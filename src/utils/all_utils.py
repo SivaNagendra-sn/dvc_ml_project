cb3d53b37047f8d6345205d4e8515c428f7a671d
SivaNagendra-sn <sivanagendra.paruchuri@gmail.com>
2021-09-30 11:10:25 +0530

    data loading & artifcats created

--- src/utils/all_utils.py
+++ src/utils/all_utils.py
@@ -4,5 +4,9 @@ import os
 def read_yaml(path_yaml: str) -> dict:
     with open(path_yaml) as ymlfile:
         content = yaml.safe_load(ymlfile)
-        
     return content
+
+def create_directory(dirs: list):
+    for dir_path in dirs:
+        os.makedirs(dir_path, exist_ok=True)
+        print(f"directory created at {dir_path}")
